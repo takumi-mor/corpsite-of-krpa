3f7a2e5bf752045051148d21fb53da1a96383225
takumi-mor <work.takumi.abys@gmail.com>
2023-01-14 19:58:52 +0900

    レスポンシブの修正

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -6,8 +6,19 @@ const Service = () => {
       <div className={classes.bg}>
         <div className={classes.section}>
           <h1>事業概要</h1>
-          <div className={classes.service}></div>
-          <div className={classes.service}></div>
+          <div className={classes.services}>
+            <div className={classes.service}>
+              <h2>製造キャリア</h2>
+              <p>
+                製造業に特化した人材紹介サービスです。
+                あなたの希望に沿った会社をご紹介します。
+              </p>
+            </div>
+            <div className={classes.service}>
+              <h2>技術キャリア</h2>
+              <p>Comming Soon</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
