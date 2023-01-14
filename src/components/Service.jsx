6c9d2c5df7ceea275e8cbc66cc4d89d4c4764dbe
takumi-mor <work.takumi.abys@gmail.com>
2023-01-15 01:26:47 +0900

    事業概要にイメージ写真を挿入

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -7,14 +7,14 @@ const Service = () => {
         <div className={classes.section}>
           <h1>事業概要</h1>
           <div className={classes.services}>
-            <div className={classes.service}>
+            <div className={classes.manuWorker}>
               <h2>製造キャリア</h2>
               <p>
                 製造業に特化した人材紹介サービスです。
                 あなたの希望に沿った会社をご紹介します。
               </p>
             </div>
-            <div className={classes.service}>
+            <div className={classes.manuEngineer}>
               <h2>技術キャリア</h2>
               <p>Comming Soon</p>
             </div>
